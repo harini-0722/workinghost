21e69b42676e0696f8e5054a011b918e85fda420
Harini <your_email@example.com>
2025-11-11 17:04:30 +0530

    last commit

---
+++ models/VisitorRequest.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+const visitorRequestSchema = new mongoose.Schema({
+    studentId: {
+        type: mongoose.Schema.Types.ObjectId, // Assumes studentId is an ObjectId
+        ref: 'Student', // Reference to your Student model
+        required: true
+    },
+    visitorName: {
+        type: String,
+        required: true
+    },
+    startDate: {
+        type: Date,
+        required: true
+    },
+    endDate: {
+        type: Date,
+        required: true
+    },
+    reason: {
+        type: String,
+        required: true
+    },
+    status: {
+        type: String,
+        enum: ['Pending', 'Approved', 'Rejected'],
+        default: 'Pending'
+    },
+    submissionDate: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('VisitorRequest', visitorRequestSchema);
